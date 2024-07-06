2edbd23d02508bb6ed4846c7f81d5aba1e1637bb
ntdolzhenko <nt.dolzhenko@gmail.com>
2024-07-06 20:27:06 +0300

    Добавлена страница с картой
    Простая карта представляющая из себя карту яндекса, с несколькими метками, можно смотреть описание меток, без другого функционала

--- Bmap/Bmap/settings.py
+++ Bmap/Bmap/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'djeym',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'djeym.middlewares.AjaxMiddleware',
 ]
 
 ROOT_URLCONF = 'Bmap.urls'
--- Bmap/main/urls.py
+++ Bmap/main/urls.py
@@ -1,8 +1,9 @@
 # main/urls.py
 from django.urls import path
-from .views import home, profile_view
+from .views import home, profile_view, maps_view
 
 urlpatterns = [
     path('', home, name='home'),
-    path('profile/', profile_view, name='profile'),  # Add other paths as needed
+    path('profile/', profile_view, name='profile'),
+    path('maps/', maps_view, name='maps'),
 ]
