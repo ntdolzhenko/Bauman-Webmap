b17af25c442a36a6dededacfd2a047d2995b4436
Ярик <yaroslav.borovikov@list.ru>
2024-07-07 22:12:56 +0300

    Создана БД
    Таблицы:
    1)Buildings - название зданий, адрес и их описание
    2)BuildingsImage - фотки зданий
    3)Favourite_Places - любимые места для пользователей

--- Bmap/main/admin.py
+++ Bmap/main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Buildings, BuildingsImage, Favourite_Places
 
+
+class BuildingImageInline(admin.TabularInline):
+    model = BuildingsImage
+    extra = 1
+
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'description')
+    inlines = [BuildingImageInline]
+
+class FavouritePlaceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'building', 'added_at', 'status')
+
+admin.site.register(Buildings, BuildingAdmin)
+admin.site.register(Favourite_Places, FavouritePlaceAdmin)
 # Register your models here.
